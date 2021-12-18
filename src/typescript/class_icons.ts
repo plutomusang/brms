815e3faab53816b982fbf8409c3e9243fca8ce4c
plutomusang <plutomusang@gmail.com>
2021-12-19 06:17:20 +0800

    icons

--- src/typescript/class_icons.ts
+++ src/typescript/class_icons.ts
@@ -22,9 +22,17 @@ import sproject from "../svg/idea_a.svg";
 import icondefault from "../svg/default.svg"
 import sicondefault from "../svg/default.svg"
 
+import brms from "../svg/brms.svg"
+import sbrms from "../svg/brms_a.svg"
 
+import bms from "../svg/bms.svg"
+import sbms from "../svg/bms_a.svg"
 
+import shakehand from "../svg/shakehand.svg"
+import sShakehand from "../svg/shakehand_a.svg"
 
+import agenda from "../svg/agenda.svg"
+import sAgenda from "../svg/agenda_a.svg"
 
 export const iconIndex =(index:number)=> {
     return  DEF_ICONS.length < index ? 8: index;
@@ -35,6 +43,6 @@ export const icon =(index:number) => {
 export const iconSmall =(index:number) => {
     return DEF_ICONSSM[iconIndex(index)];
 }
-export const DEF_ICONSSM=  [undefined         ,sdocument, stransmittal       , sproject  , srun      , sidea           ,sprocess       , sdelivery        ,sicondefault];
-export const DEF_ICONS =   [undefined         ,document , transmittal        , project   , run       , idea            ,process        , delivery         ,icondefault];
-export const DEF_DOCTYPE = ['Track New Record', 'ARO'   , 'Letter of Request', 'Incoming', 'Outgoing', 'Project Design','Sports Design', 'Training Design',''];
+export const DEF_ICONSSM=  [undefined         ,sdocument, stransmittal       , sproject  , srun      , sidea           ,sprocess       , sdelivery        ,sicondefault, sbrms, sbms, sShakehand, sAgenda];
+export const DEF_ICONS =   [undefined         ,document , transmittal        , project   , run       , idea            ,process        , delivery         ,icondefault, brms, sbms, shakehand, agenda];
+export const DEF_DOCTYPE = ['Track New Record', 'ARO'   , 'Letter of Request', 'Incoming', 'Outgoing', 'Project Design','Sports Design', 'Training Design','', 'BRMS', 'BMS', 'LFC Agenda'];
